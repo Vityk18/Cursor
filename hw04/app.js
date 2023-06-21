02ccdb5fb5b81267486a2d2d8aca7e4c5a50625f
Vityk18 <vityk18@gmail.com>
2023-06-21 14:45:59 +0300

    hw04

--- hw04/app.js
+++ hw04/app.js
@@ -1,80 +1,107 @@
-const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій",
-"Світлана"];
-const themes = ["Диференційне рівняння", "Теорія автоматів",
-"Алгоритми і структури даних"];
+const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
+const themes = [
+  "Диференційне рівняння",
+  "Теорія автоматів",
+  "Алгоритми і структури даних",
+];
 const marks = [4, 5, 5, 3, 4, 5];
 
+import { maleList } from "./maleNamesList.js";
+import { femaleList } from "./femaleNamesList.js";
+
+const maleArr = maleList.replaceAll("\r", "").split("\n");
+let maleNamesArr = [];
+for (let element of maleArr) {
+  if (element.length > 1) {
+    maleNamesArr.push(element);
+  }
+}
+
+const femaleArr = femaleList.replaceAll("\r", "").split("\n");
+let femaleNamesArr = [];
+for (let element of femaleArr) {
+  if (element.length > 1) {
+    femaleNamesArr.push(element);
+  }
+}
 
 // 1. Розділіть студентів на пари(хлопець + дівчина) для работи над
 // проєктом.У вас повинен вийти вкладений масив з парами студентів:
 // [["Олександр", "Олена"], [..], [...]];
 
+const pairArr = [];
 
+const pair = (students) => {
+  let boyArr = [];
+  let girlArr = [];
 
-fetch('maleNamesList.txt')
-  .then(response => response.text())
-  .then(data => {
-    // Отримання тексту з файлу імен
-   const names = data.replaceAll("\r", "").split('\n');
-   let maleNames = []
-   for (let i = 0; i < names.length; i++) {
-    if (names[i].length > 1) {
-        maleNames.push(names[i])
+  for (let element of students) {
+    if (maleNamesArr.includes(element)) {
+      boyArr.push(element);
+    } else if (femaleArr.includes(element)) {
+      girlArr.push(element);
+    } else {
+      return `введене імя "${element}" не знайдено. Додайте його в чоловічий, або жіночий список`;
     }
-   }
+  }
 
-    // console.log(maleNames); // Виведення списку імен у консоль
-    // Ваш код для роботи зі списком імен
-  })
-  .catch(error => console.error('Сталася помилка під час завантаження файлу:', error));
+  for (let i = 0; i < boyArr.length; i++) {
+    pairArr.push(`${boyArr[i]} і ${girlArr[i]}`);
+  }
 
-const funnc = (bg) =>
-console.log(bg);
-
-funnc(maleNames)
-
-const pairArr = (stud) => {
-
-   
-
-//    const maleNamesArr = maleNamesList.split(`\n`)
-//    let maleNames = []
-//    for (i = 0; i < maleNamesArr.length; i++) {
-//        if (maleNamesArr[i].length > 1) {
-//         maleNames.push(maleNamesArr[i])
-//        }
-//    }
-// return maleNames
-
-
-
-
-}
-console.log(pairArr(students));
-
-
+  return pairArr;
+};
+console.log(pair(students));
 
 // 2. Зіставте пари з попереднього завдання та теми проєктів, над
 // якими студенти будуть працювати.
 // Повинен вийти вкладений масив виду: [["Олександр і Олена",
 // "Теорія автоматів"], [...], [...]]
 
-const studentsWithProject = [`${pairArr[0]}, ${themes[0]}`, `${pairArr[1]}, ${themes[1]}`, `${pairArr[2]}, ${themes[2]}`, ]
+let allStudentsWithProject = [];
+const studentsWithProject = (pair, theme) => {
+  let studentsWithProject = [];
 
-console.log(studentsWithProject);
+  for (let i = 0; i < pairArr.length; i++) {
+    studentsWithProject = [`${pairArr[i]}, ${themes[i]}`];
+    allStudentsWithProject.push(studentsWithProject);
+  }
 
-
+  return allStudentsWithProject;
+};
+console.log(studentsWithProject(pairArr, themes));
 
 // 3. Зіставте оцінки(marks) зі студентом(students): [["Саша", 4], [...],
 // [...]]
 
-// const studentMark = [students + marks]
+function studentsMark() {
+  const arr = [...marks];
+  let studentsMarks = [];
+  for (let i = 0; i < marks.length; i++) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    let studentMark = [students[i], arr[randomIndex]];
+    studentsMarks.push(studentMark);
+    const removedElement = arr.splice(randomIndex, 1);
+  }
+  return studentsMarks;
+}
 
-// console.log(studentMark);
+console.log(studentsMark());
 
+// 4. Поставте кожній парі випадкову оцінку(від 1 до 5) за проєкт
+// (тут функція буде не чистою, але не повинна мутувати массив):
+// [["Олександр і Олена", "Теорія автоматів", 5], [...], [...]]
 
-console.log(`-----------------------------check line------------------------------`);
-console.log(students);
-console.log(themes);
-console.log(marks);
+let pairWithRandoomMarks = () => {
+  const allPairsWithRandoomMarks = [];
+  const pairWithRandoomMarks = [];
+  for (let pair of allStudentsWithProject) {
+    const randoomMark = Math.floor(Math.random() * (5 - 1 + 1) + 1);
+    pairWithRandoomMarks.push([`${pair}, ${randoomMark}`]);
+    // allPairsWithRandoomMarks.push(pairWithRandoomMarks)
+  }
 
+  return pairWithRandoomMarks;
+};
+
+console.log(pairWithRandoomMarks());
