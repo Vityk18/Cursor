52731b455fff9e5c1e731cb9c8a91eab877e8d29
Vityk18 <vityk18@gmail.com>
2023-07-17 08:34:49 +0300

    hw11

--- hw11/app.js
+++ hw11/app.js
@@ -1,29 +1,27 @@
 let length;
-const butt = document.querySelector('#send');
+const butt = document.querySelector("#send");
 butt.onclick = function () {
-  const length = document.querySelector('#input').value;
-  getRandomChinese(length)
-}
-console.log(length);
+  length = document.querySelector("#input").value;
+  getRandomChinese(length);
+};
 
 function getRandomChinese(leng) {
-const promice = new Promise((resolve) => {
-  resolve()
-})
+  const delay = () =>
+    new Promise((resolve) => {
+      setTimeout(() => resolve(), 50);
+    });
 
-let i = 0
-let result = ''
-while (i < leng) {
-  promice.then(() => {
-   const date = new Date().getTime().toString()
-   const sign = date[8] + date[9] + date[10] + date[11] + date[12]
-   result += String.fromCharCode(sign);
-    document.getElementById("myOutput").innerHTML = result
-  })
-  i++
+  async function getSymbol() {
+    let result = "";
+    while (leng > 0) {
+      await delay();
+      const date = new Date().getTime().toString();
+      const sign = date[8] + date[9] + date[10] + date[11] + date[12];
+      result += String.fromCharCode(sign);
+      document.getElementById("myOutput").innerHTML = result;
+      console.log(leng);
+      leng--;
+    }
+  }
+  getSymbol();
 }
-}
-
-
-
-
